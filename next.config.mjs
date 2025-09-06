af38fcfd692c80cda92dda685421a6da491aa6c5
Matt Thee Dev <pacificrobinson@gmail.com>
2025-09-06 15:03:42 -0700

    updated with correct configuration

--- next.config.mjs
+++ next.config.mjs
@@ -6,9 +6,13 @@ const nextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
+  basePath: '/zshot-fe',
+  assetPrefix: '/zshot-fe/',  // Added trailing slash
+  trailingSlash: true,        // Added this
   images: {
     unoptimized: true,
   },
+  output: 'export'
 }
 
 export default nextConfig
